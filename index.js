86e2c38f6e37ffde8f7e6a8c967cf9d338d88149
mihiran <154015E@uom.lk>
2021-02-04 11:23:27 +0530

    pass the array of element to react.createElement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const  reactContentRoot = document.getElementById('root')
 
 const myFirstElement = React.createElement("ul",null,
-      React.createElement("li",null,"item1") 
+     [ React.createElement("li",null,"item1") , React.createElement("li",null,"item2") ]
    )
 
 ReactDOM.render(myFirstElement,reactContentRoot)
