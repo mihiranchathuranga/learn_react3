7651fd6a647a6a259e5453064d1f0445bd9b5e61
mihiran <154015E@uom.lk>
2021-02-04 12:14:19 +0530

    changes to ReactDOM.render

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const  reactContentRoot = document.getElementById('root')
 
-const myFirstElement = React.createElement("ul",null,
-     [ React.createElement("li",null,"item1") , React.createElement("li",null,"item2") ]
+/*const myFirstElement = React.createElement("ul",null,
+     [ React.createElement("li",null,"item1") ,
+      React.createElement("li",null,"item2") ]
+   )*/
+
+   //how to avoid the using of react.createElement
+
+   const myJSXElement = (
+       <ul>
+           <li>item1</li>
+           <li>item2</li>
+       </ul>
    )
 
-ReactDOM.render(myFirstElement,reactContentRoot)
+ReactDOM.render(myJSXElement,reactContentRoot)
