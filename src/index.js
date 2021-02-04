8c36f15f2e704d3d38444323a0373032cf1dd4ab
mihiran <154015E@uom.lk>
2021-02-04 16:47:21 +0530

    import App.js to index.js and render it

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
 const  reactContentRoot = document.getElementById('root')
-
-   const App = () => {
-
-    const myItem = "item_"
-       return(
-        <ul>
-        <li>item1</li>
-        <li>item2</li>
-        <li>{myItem}</li>
-    </ul>
-       )
-   }
-
-   
-
 ReactDOM.render(<App/>,reactContentRoot)
