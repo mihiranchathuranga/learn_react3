cef0b9dd6e6581966c669abfa4f1a954299a066c
mihiran <154015E@uom.lk>
2021-02-04 17:20:08 +0530

    define the const CurrentCount

--- src/components/CountButton.js
+++ src/components/CountButton.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 
 const CountButton = () => {
+    const currentCount = 0
     return <div>
         <button>+1</button>
-        <div>0</div>
+        <div>{currentCount}</div>
     </div>
 }
 
