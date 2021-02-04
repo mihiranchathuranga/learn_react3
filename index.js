6db0bee09fe1948ea716c00bd0d25b699fe4cece
mihiran <154015E@uom.lk>
2021-02-04 11:19:48 +0530

    change the myFirstElement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const  reactContentRoot = document.getElementById('root')
 
-const myFirstElement = React.createElement("li",null,"item1")
+const myFirstElement = React.createElement("ul",null,
+      React.createElement("li",null,"item1") 
+   )
 
 ReactDOM.render(myFirstElement,reactContentRoot)
