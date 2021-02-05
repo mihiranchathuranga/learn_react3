aca12944449f1d345b62d8b13d6f783f3bb5e9cf
mihiran <154015E@uom.lk>
2021-02-05 15:58:06 +0530

    create handleInputChangeFunction and onChange

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,9 +4,13 @@ import './SearchBar.css'
 const SearchBar = () =>{
 const searchValue = "the search value"
 
+const handleInputChange = () => {
+    alert("Changed")
+}
+
    return(
        <div>
-           <input type="text" value={searchValue}/>
+           <input type="text" value={searchValue} onChange={handleInputChange}/>
        </div>
    )
 }
