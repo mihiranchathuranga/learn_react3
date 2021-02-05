374c771c8dd131c02ed3b9d9a7e7a2fd20a64391
mihiran <154015E@uom.lk>
2021-02-05 15:49:43 +0530

    add the input field,define new variable

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,8 +2,12 @@ import React from 'react'
 import './SearchBar.css'
 
 const SearchBar = () =>{
+const searchValue = "the search value"
+
    return(
-       <div>Search Bar</div>
+       <div>
+           <input type="text" value={searchValue}/>
+       </div>
    )
 }
 
