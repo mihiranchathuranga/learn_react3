6d10a693c89e5a5fa6e4c63d6f84ba933b789b0a
mihiran <154015E@uom.lk>
2021-02-05 16:10:37 +0530

    write new function setSearchValue and useState

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchBar.css'
 
 const SearchBar = () =>{
-const searchValue = "the search value"
+const [searchValue,setSearchValue] = useState('the search value')
 
 const handleInputChange = (event) => {
    // alert("Changed")
-   console.log(event.target.value)
+   setSearchValue(event.target.value) //try to set the value for searchValue
 }
 
    return(
        <div>
-           <input type="text" value={searchValue} onChange={handleInputChange}/>
+           <input type="text" value={searchValue} onChange={handleInputChange}/>{searchValue}
        </div>
    )
 }
