521d729e955b96adfc6e3f008c81065fd961d376
mihiran <154015E@uom.lk>
2021-02-05 22:48:20 +0530

    --

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,26 @@ import React from 'react'
 import CountButton from './CountButton/CountButton'
 import SearchBar from './SearchBar/SearchBar'
 
-const products = [
+
+
+const App = () => {   
+       return(
+        <div>
+            <SearchBar products={[
     'tooth paste',
     'tooth brush',
     'mouth wash',
     'dental floss',
     'mouth guard'
-]
+]}/>
 
-const App = () => {   
-       return(
-        <div>
-            <SearchBar products={products}/>
+<SearchBar products={[
+    'bike',
+    'mountain bike',
+    'soccer',
+    
+]}/>
+
         </div>
        )
    }
